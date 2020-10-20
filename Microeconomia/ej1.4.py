deaf6c9c0c1ee08e9bf0bd1223576a1fb2a0ad83
LuciaFresno <luciafresno@gmail.com>
2020-10-19 23:27:00 -0300

    detalles teoricos ej1.4

--- Microeconomia/ej1.4.py
+++ Microeconomia/ej1.4.py
@@ -16,7 +16,7 @@ m, x, y, px, py, t = sp.symbols('m,x,y,px,py,t')
 Presupuesto = m - px*x - py*y
 TMS = 2y/x
 # RMS o TMS Si en la representación de las curvas de indiferencia y de la relación marginal de sustitución, el bien 1 representa el consumo de "todos los demás bienes" y se mide en la cantidad de $ que podemos gastar en ellos, la relación marginal de sustitución se entendería como la cantidad que el consumidor está dispuesto a pagar por una unidad adicional del bien 2. (Costo de oportunidad)
-
+# la pendiente de la curva de indiferencia, la relación marginal de sustitución mide la relación en la que al consumidor le es igual intercambiar o no los dos bienes. Con cualquier otra relación de intercambio que no sea la relación marginal de sustitución, deseará intercambiar un bien por el otro. Pero si la relación de intercambio es idéntica a la relación marginal de sustitución, deseará permanecer en el mismo punto.
 
 
 im =  180
@@ -31,7 +31,7 @@ def canastaOptima(TMS,
                   ipy):
     # TMS(x,y) = sp.Eq((dy/dx), (UMgx/UMgy)) 
     # 
-
+    pass
 
 canastaOptima(TMS, Presupuesto , im , ipx , ipy)
 
